741daa5b6d998efe44a4eeebf8506e6b565c60ec
Andrew Yuan <andrew.yuan@cooper.edu>
2022-09-07 23:45:21 +0000

    Lab 2 stuff

---
+++ cs102/lab-2/lab3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main( int argc, char **argv )
+{
+int i;
+for ( i=0;i<10;i = i + 1) 
+fprintf (stdout, "Hello World!\n");
+
+return 0;
+}
---
+++ cs102/lab-2/lab5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main( int argc, char **argv )
+{
+int i;
+for ( i=0;i<10;i = i + 1)
+fprintf (stdout, "Hello World!\n");
+
+fprintf (stdout, "Lovely Weather!\n");
+
+int x;
+for (x=0; x<10; x = x + 1)
+fprintf (stdout, "Goodbye World!\n");
+return 0;
+}
