f25442eba2682b2382e38fe2dff593f1184f3e4f
Andrew Yuan <andrew.yuan@cooper.edu>
2022-09-07 22:55:01 +0000

    week2 labs

---
+++ cs102/lab-2/lab2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main( int argc, char **argv )
+{
+fprintf (stdout, "Hello World!\n");
+fprintf (stdout, "Hello World!\n");
+fprintf (stdout, "Hello World!\n");
+fprintf (stdout, "Hello World!\n");
+fprintf (stdout, "Hello World!\n");
+fprintf (stdout, "Hello World!\n");
+fprintf (stdout, "Hello World!\n");
+fprintf (stdout, "Hello World!\n");
+fprintf (stdout, "Hello World!\n");
+fprintf (stdout, "Hello World!\n");
+return 0;
+}
