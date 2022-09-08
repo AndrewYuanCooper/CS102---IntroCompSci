d96bb06965421379046b21577fc8eb30773c546b
Andrew Yuan <andrew.yuan@cooper.edu>
2022-09-08 00:33:09 +0000

    homework 3/4

---
+++ cs102/homework-2/repetitive.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main( int argc, char **argv )
+{
+int i;
+fprintf (stdout, "Andrew Yuan 9/7/2022 102C2022Fall\n");
+
+for ( i=0;i<25;i = i + 1)
+fprintf (stdout, "Punch\nKick\n");
+
+fprintf (stdout, "Take a deep breath\n");
+
+int x;
+for (x=0; x<25; x = x + 1)
+fprintf (stdout, "Punch\nKick\n");
+return 0;
+}
+
