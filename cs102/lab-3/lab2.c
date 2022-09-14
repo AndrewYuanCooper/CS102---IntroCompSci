50d3233658e9e5602259a0f6564fcd00b2f71c04
806-15 <andrew.yuan@cooper.edu>
2022-09-14 19:19:52 -0400

    this is awesome i finished lab2

---
+++ cs102/lab-3/lab2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main( int argc, char **argv )
+{
+int x = 0;
+int y = 0;
+int h = 2;
+int yprime = 0;
+
+fprintf ( stdout, "x,y,yprime\n" );
+while( x <= 200 )
+{
+y = 1 + x*x;
+yprime = ((1+x*x)-(1+(x-h)*(x-h)))/h;
+
+fprintf( stdout, "%d;%d;%d\n",x,y,yprime);
+x = x + 1;
+}
+return 0;
+}
