006ee47ac30c2027339ac6d1f7ac301ad267db41
Andrew Yuan <andrew.yuan@cooper.edu>
2022-09-15 01:59:23 +0000

    homework4

--- cs102/homework-3/graph.c
+++ cs102/homework-3/graph.c
@@ -3,18 +3,21 @@ int main( int argc, char **argv )
 {
 int x = 0;
 int y = 0;
-int h = 2;
+int h = 4;
 int yint = 0;
 int yprime = 0;
-fprintf ( stdout, "x,y,yprime\n" );
+int ysum = 0;
+
+fprintf ( stdout, "x,y,yprime,ysum\n");
 while( x <= 200 )
 {
-y = 1 + x*x;
+y = 6*x*x*x - 4*x*x + 8;
 yprime = ((1+x*x)-(1+(x-h)*(x-h)))/h;
 yint = (((1+x*x)+(1+(x-h)*(x-h)))/2)*h;
+ysum = ysum + yint;
 
-fprintf( stdout, "%d;%d;%d;%d\n",x,y,yprime,yint);
-x = x + 1;
+fprintf( stdout, "%d;%d;%d;%d\n",x,y,yprime,ysum);
+x = x + h;
 }
 return 0;
 }
