55906c909fcaa4a1d050c06b2249cfe35c96e565
Andrew Yuan <Andrew.yuan@cooper.edu>
2022-09-21 00:20:38 +0000

    homework3

--- cs102/homework-3/graph.c
+++ cs102/homework-3/graph.c
@@ -12,8 +12,8 @@ fprintf ( stdout, "x,y,yprime,ysum\n");
 while( x <= 100 )
 {
 y = 6*x*x*x - 4*x*x + 8;
-yprime = ((1+x*x)-(1+(x-h)*(x-h)))/h;
-yint = (((1+x*x)+(1+(x-h)*(x-h)))/2)*h;
+yprime = ((6*x*x*x - 4*x*x + 8) - (6*(x-h)*(x-h)*(x-h)- 4*(x-h)*(x-h) + 8))/h ;
+yint = (((6*x*x*x - 4*x*x + 8) + (6*(x-h)*(x-h)*(x-h)- 4*(x-h)*(x-h) + 8))/2)*h;
 ysum = ysum + yint;
 
 fprintf( stdout, "%d;%d;%d;%d\n",x,y,yprime,ysum);
