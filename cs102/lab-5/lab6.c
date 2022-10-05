80b3a0095ffbc7794a7a11fb6903a20a3e990b53
Andrew Yuan <Andrew.yuan@cooper.edu>
2022-10-05 17:45:29 +0000

    lab6done

--- cs102/lab-5/lab6.c
+++ cs102/lab-5/lab6.c
@@ -2,16 +2,19 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include "lab5func.h"
+#include "lab6func.h"
 
 int main( int argc, char **argv ){
 	int array[5][5];
 	int array2[5][5];
+	int array3[5][5];
 	srand48(getpid());
 	fill(array);
 	print(array);
 	fill(array2);
 	print(array2);
+	add(array, array2, array3);
+	print(array3);
 	return 0;
 }
 
--- cs102/lab-5/lab6func.c
+++ cs102/lab-5/lab6func.c
@@ -2,13 +2,12 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include "lab5func.h"
+#include "lab6func.h"
 
 void fill( int array[5][5]){
 
-
         int random;
-                srand48(getpid());
+               
 
         int i = 0;
 	int j = 0;
@@ -41,3 +40,18 @@ void print( int array[5][5]){
 	}
 }
 
+void add(int array[5][5], int array2[5][5], int array3[5][5]){
+
+        int i = 0;
+        int j = 0;
+
+        for( i = 0; i<5; i=i+1){
+
+
+                for (j = 0; j<5; j = j+1)
+                {
+                	array3[i][j] = array[i][j] + array2[i][j];
+               	}
+        }
+
+}
