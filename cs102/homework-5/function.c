f786bcd79f800dbbc8bff0c5db242a8fba045d8d
Andrew Yuan <Andrew.yuan@cooper.edu>
2022-10-05 18:50:49 +0000

    Homework5done

---
+++ cs102/homework-5/function.c
@@ -0,0 +1,99 @@
+#include "function.h"
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void fill8x12( int array[8][12] ) {
+
+  int random;
+  for(int i = 0; i < 8; i=i+1) {
+
+    for(int j = 0; j < 12; j=j+1) {
+
+		random = mrand48()%100;
+		if (random < 0){
+			random = random - random - random;
+		}
+		array[i][j] = random;  
+    }
+  }
+}
+
+void fill12x8( int array[12][8] ) {
+  int random;
+  for(int i = 0; i < 12; i=i+1) {
+
+    for(int j = 0; j < 8; j=j+1) {
+
+		random = mrand48()%100;
+		if (random < 0){
+			random = random - random - random;
+		}
+		array[i][j] = random; 
+    }
+  }
+}
+
+
+void print8x12(int array[8][12] ) {
+
+  for(int i = 0; i < 8; i=i+1) {
+
+    for(int j = 0; j < 12; j=j+1) {
+
+      fprintf(stdout,"%5d", array[i][j]); 
+    }
+	fprintf(stdout,"\n");
+  }
+}
+
+
+
+void print12x8(int array[12][8] ) {
+
+  for(int i = 0; i < 12; i=i+1) {
+
+    for(int j = 0; j < 8; j=j+1) {
+
+      fprintf(stdout,"%5d", array[i][j]); 
+    }
+	fprintf(stdout,"\n");
+  }
+}
+
+
+
+void print12x12(int array[12][12] ) {
+
+  for(int i = 0; i < 12; i=i+1) {
+
+    for(int j = 0; j < 12; j=j+1) {
+
+      fprintf(stdout,"%10d", array[i][j]); 
+    }
+	fprintf(stdout,"\n");
+  }
+}
+
+void cross(int array[8][12], int array2[12][8], int array3[12][12] ) {
+
+  for(int i = 0; i < 12; i=i+1) {
+
+    for(int j = 0; j < 12; j=j+1) {
+
+      array3[i][j] = 0; 
+    }
+  }
+
+  for(int i = 0; i < 12; i=i+1) {
+
+    for(int j = 0; j < 12; j=j+1) {
+
+      for(int k = 0; k < 8; k=k+1) {
+        
+        array3[i][j] = array3[i][j] + array2[j][k] * array[k][i]; 
+      }
+    }
+  }
+}
---
+++ cs102/homework-5/main.c
@@ -0,0 +1,22 @@
+#include "function.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+int main(int argc, char **argv) {
+  int array[8][12];
+  int array2[12][8];
+  int array3[12][12];
+  srand48(getpid());
+  fill8x12(array);
+  fill12x8(array2);
+  cross(array, array2, array3);
+
+  print8x12(array);
+  printf("\n");
+  print12x8(array2);
+  printf("\n");
+  print12x12(array3);
+  printf("\n");
+  return 0;
+}
